a32603f82d7641564e104b93639a572934a0cc62
EC2 Default User <ec2-user@ip-172-31-24-95.us-west-1.compute.internal>
2020-10-30 06:49:01 +0000

    adding first page

--- index.php
+++ index.php
@@ -1,9 +1,39 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title> Lab 1: Personal Page</title>
+        <meta charset='utf-8'
     </head>
     <body>
-        <h1>Welcome to my website!</h1>
+        
+        <header>
+           <h1> Hugo Rodriguez </h1>
+        </header>
+
+    <hr>
+    
+    <nav>
+        <a href="index.html"> Home </a>
+        <a href="about.html"> About </a>
+        <a href="contact.html"> Contact Me </a>
+    </nav>
+    
+    <main>
+        
+        <figure>
+            <img src="">        
+        </figure>
+        
+        <div>
+            Welcome to my site!    
+        </div>
+        
+    
+    </main>
+
+    <footer>
+        
+    </footer>    
+    
     </body>
 </html>
